fa42c73b69483aa19d6d31815a869e86a6efc42e
jp3parker <jp3parker@gmail.com>
2023-04-19 16:10:33 -0500

    added the background and displaying it to the screen, also set frame rate limit to 30

--- main.cpp
+++ main.cpp
@@ -2,22 +2,36 @@
 #include "main.h"
 
 int main(int argc, char* argv[]) {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(600, 1200), "SFML works!");
+  window.setFramerateLimit(30);
   
-  while (window.isOpen())
-  {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      if (event.type == sf::Event::Closed)
-        window.close();
-    }
-    
-    window.clear();
-    window.draw(shape);
-    window.display();
+  std::string FailureToLoadError = "";
+  
+  sf::Texture BackgroundTexture;
+  if (!BackgroundTexture.loadFromFile("tetris-background.png")) {
+    FailureToLoadError += "Could not load tetris-background.png\n";
   }
+  
+  if (FailureToLoadError == "") { // everything loaded successfully
+    sf::Sprite Background;
+    Background.setTexture(BackgroundTexture);
+  
+    while (window.isOpen()) {
+      sf::Event event;
+      while (window.pollEvent(event))
+      {
+        if (event.type == sf::Event::Closed)
+          window.close();
+      }
+      
+      window.clear();
+      window.draw(Background);
+      window.display();
+    }
+  }
+  else { // at least one image failed to load
+    std::cout << FailureToLoadError << std::endl;
+  }
+  
   return 0;
 }
--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 #define MAIN_HEADER
 
 #include <iostream>
+#include "string.h"
 #include <SFML/Graphics.hpp>
 
 #endif //MAIN_HEADER
