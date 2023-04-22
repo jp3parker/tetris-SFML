d163ea2372e85dc44ea87f42eac31ed1c3acaac1
jp3parker <jp3parker@gmail.com>
2023-04-21 19:41:31 -0500

    loaded the 7 different squares and made texture variables

--- main.cpp
+++ main.cpp
@@ -9,7 +9,35 @@ int main(int argc, char* argv[]) {
   
   sf::Texture BackgroundTexture;
   if (!BackgroundTexture.loadFromFile("images/tetris-background.png")) {
-    FailureToLoadError += "Could not load tetris-background.png\n";
+    FailureToLoadError += "Could not load images/tetris-background.png\n";
+  }
+  sf::Texture YellowTexture;
+  if (!YellowTexture.loadFromFile("images/yellow-square.png")) {
+    FailureToLoadError += "Could not load images/yellow-square.png\n";
+  }
+  sf::Texture RedTexture;
+  if (!RedTexture.loadFromFile("images/red-square.png")) {
+    FailureToLoadError += "Could not load images/red-square.png\n";
+  }
+  sf::Texture PurpleTexture;
+  if (!PurpleTexture.loadFromFile("images/purple-square.png")) {
+    FailureToLoadError += "Could not load images/purple-square.png\n";
+  }
+  sf::Texture OrangeTexture;
+  if (!OrangeTexture.loadFromFile("images/orange-square.png")) {
+    FailureToLoadError += "Could not load images/orange-square.png\n";
+  }
+  sf::Texture LightBlueTexture;
+  if (!LightBlueTexture.loadFromFile("images/light-blue-square.png")) {
+    FailureToLoadError += "Could not load images/light-blue-square.png\n";
+  }
+  sf::Texture GreenTexture;
+  if (!GreenTexture.loadFromFile("images/green-square.png")) {
+    FailureToLoadError += "Could not load images/green-square.png\n";
+  }
+  sf::Texture DarkBlueTexture;
+  if (!DarkBlueTexture.loadFromFile("images/dark-blue-square.png")) {
+    FailureToLoadError += "Could not load images/dark-blue-square.png\n";
   }
   
   if (FailureToLoadError == "") { // everything loaded successfully
